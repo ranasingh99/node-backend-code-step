4c069f2d41123a107964bd980bc591c37ad60149
ranasingh99 <sanketverma852@gmail.com>
2023-08-18 12:32:50 +0530

    basic import and export modules-eg1

---
+++ main.js
@@ -0,0 +1,7 @@
+// import content from another file to this file 
+// always import in the form of objects.
+// eg-1;
+const index = require('./index')
+let a = index.x;
+let b = index.y;
+console.log(a+b);
